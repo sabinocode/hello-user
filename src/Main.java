d43716c980043d97cdb143e452db1cb75c0e6592
marcello <sabinocode@gmail.com>
2016-01-13 17:15:15 -0500

    Created a GUI
    Added a GUI so that the application will be able to run through a .jar
    file.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,40 @@
-import java.util.Scanner;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 public class Main {
 	
-	static Scanner scanner = new Scanner(System.in);
+	private final String NAME = "Hello-User";
+	
+	private String fname;
+	private JFrame frame;
+	private JPanel panel;
+	
+	/**
+	 * Main class constructor.
+	 */
+	public Main() {
+		frame = new JFrame(NAME);
+		panel = new JPanel();
+		
+		frame.add(panel);
+		frame.setPreferredSize(new Dimension(200, 200));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		
+		frame.setLayout(new BorderLayout());
+		frame.add(panel, BorderLayout.CENTER);
+		
+		frame.pack();
+		frame.setVisible(true);
+		
+		init();
+	}
 
 	/**
 	 * Prompts the user to enter his or her first name.
@@ -10,9 +42,16 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.print("Please enter your first name: ");
-		String name = scanner.nextLine();
+		new Main();
+	}
+	
+	/**
+	 * Initializer method.
+	 */
+	public void init() {
+		fname = JOptionPane.showInputDialog("What's your first name?");
+		panel.add(new JLabel("Hello " + fname + "."));
 		
-		System.out.println("Hello " + name + ".");
+		panel.validate();
 	}
 }
